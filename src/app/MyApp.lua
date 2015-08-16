bac1d88e3ec9062eb045e04feab4fecc9d08ea02
Yuanfei He <hyf@juhuiwan.cn>
2015-08-16 15:31:17 +0800

    增加服务器关闭提示

--- src/app/MyApp.lua
+++ src/app/MyApp.lua
@@ -1,13 +1,18 @@
 
 local MyApp = class("MyApp", cc.load("mvc").AppBase)
 local ServieceSocket = cc.load("mvc").ServieceSocket
-
+local Pop = import(".views.Popu")
 function MyApp:onCreate()
     math.randomseed(os.time())
     self.socket_=ServieceSocket:create()
 self.socket_:conn("ws://192.168.3.62:8080",function()
         print("Connect Success")
 --        self.socket_:send(1001,{t="Hello"})
+    end,function()
+        local pop =Pop:create()
+        pop:getTipsTxt():setString("服务器关闭!!")
+        local scene=cc.Director:getInstance():getRunningScene();
+        scene:addChild(pop)
     end)
 end
 
